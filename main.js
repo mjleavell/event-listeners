c52237a40d009cf036d337c9fcb901e2d7447a0a
Maggie Leavell <mjleavell@gmail.com>
2018-09-18 19:47:17 -0500

    done adding card

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 // VARIABLES
 const toDoInputElem = document.getElementById('toDoInput');
 const notesInputElem = document.getElementById('notesInput');
+
 const submitToDoButtonElem = document.getElementById('submitToDoButton');
+const removeToDoButtonElem = document.getElementById('removeToDoButton');
 
 const printToDom = (stringToPrint, whereToPrint) => {
     document.getElementById(whereToPrint).innerHTML += stringToPrint;
 }
 
 const buildNewToDoCard = (toDo, notes) => {
-    let domString = '<h1>hello</h1>';
+    let domString = `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${toDo}</h5>
+      <p class="card-text">${notes}</p>
+      <a href="#" id="removeToDoButton" class="btn btn-primary">Remove Card</a>
+    </div>
+  </div>`;
 
     printToDom(domString, 'toDoCards');
 }
 
 
+
 submitToDoButtonElem.addEventListener('click', (e) => {
     e.preventDefault();
-
-
-
     // .value is how we capture information from forms
     buildNewToDoCard(toDoInputElem.value, notesInputElem.value);
 });
